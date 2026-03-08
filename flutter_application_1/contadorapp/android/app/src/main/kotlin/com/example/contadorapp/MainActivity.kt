4d34992d004f024e8d651f7702807de5eeab06d1
Edson Aguirre <020200141d@uandina.edu.pe>
2026-03-08 17:26:17 -0500

    reemplazando repositorio

--- flutter_application_1/contadorapp/android/app/src/main/kotlin/com/example/contadorapp/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.contadorapp
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- flutter_application_1/inmobidart/test/inmobidart_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:inmobidart/inmobidart.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
